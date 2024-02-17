38f74a3c743e963e305e7b20c0e7413a6e983efe
Adem Kedir <ademkedir724@gmail.com>
2024-02-17 11:46:35 +0300

    create project  and projectItem class

---
+++ src/app/ProjectItem.js
@@ -0,0 +1,23 @@
+class projectsItem {
+  #tittle;
+  #description;
+  #date;
+  #category;
+  #priority;
+  #isDone;
+  constructor(tittle, description = "", date, category, priority = 3) {
+    this.#tittle = tittle;
+    this.#description = description;
+    this.#date = date;
+    this.#category = category;
+    this.#priority = priority;
+    this.#isDone = false;
+  }
+  isDone() {
+    this.#isDone = this.#isDone ? false : true;
+  }
+}
+
+const item1 = new projectsItem("read", "lkdjfskl", "jun 3:2022", "study", 2);
+const item2 = new projectsItem("reading", "lkdjfskl", "jun 3:2022", "study", 2);
+console.log(item1);
