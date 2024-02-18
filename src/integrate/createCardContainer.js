876aaf44b35f5d7c75a0be4db6fbd62a42771dc2
Adem Kedir <ademkedir724@gmail.com>
2024-02-18 22:56:05 +0300

    render by default project[0] items

--- src/integrate/createCardContainer.js
+++ src/integrate/createCardContainer.js
@@ -1,7 +1,17 @@
 import { Project } from "../app/Project";
 import { TaskCard } from "../dom/createTaskCard";
 
+// render Default
+export function defaultRender() {
+  const displayCard = document.querySelector(".card-display");
+  Project.projects[0].projectsItem.forEach((obj) => {
+    let card = TaskCard.createCard(obj);
+    displayCard.appendChild(card);
+  });
+}
 export function renderTasks(ev) {
+  console.log(ev);
+  removeCards();
   const displayCard = document.querySelector(".card-display");
   const currentProject =
     Project.projects[ev.currentTarget.dataset.project].projectsItem;
@@ -10,6 +20,15 @@ export function renderTasks(ev) {
     let card = TaskCard.createCard(obj);
     displayCard.appendChild(card);
   });
+}
+
+function removeCards() {
+  const displayCard = document.querySelector(".card-display");
+  const displays = document.querySelector(".display");
+  displays.removeChild(displayCard);
+  let newDisplayCard = document.createElement("div");
+  newDisplayCard.classList.add("card-display");
+  displays.appendChild(newDisplayCard);
 }
 
 function displayAllProject() {
