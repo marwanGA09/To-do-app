37f067b5d74373ab0a253ed7aaddab579015ecd7
Adem Kedir <ademkedir724@gmail.com>
2024-02-18 17:33:37 +0300

    modify task creation method

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import { addItem } from "./integrate/taskCreator.js";
 import { TaskCard } from "./dom/createTaskCard.js";
 import "./integrate/createCardContainer.js";
 const modal = document.querySelector(".modal");
+
 // modal.showModal();
 
 // console.log(Project.projects);
-
-// date.toLocaleDateString(); // 5/12/2020
-// date.toLocaleTimeString(); // 6:50:21 PM
