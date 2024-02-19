8f304ede9c17a93ff9bb225f46e4eafa390b5ad1
Adem Kedir <ademkedir724@gmail.com>
2024-02-19 10:37:39 +0300

    display input box for intering tasks

--- src/integrate/taskCreator.js
+++ src/integrate/taskCreator.js
@@ -2,19 +2,24 @@ import { Project } from "../app/Project";
 import { TaskClass } from "./../app/task.js";
 
 function createTaskFunction(ev) {
-  const currentProject = Project.projects[ev.target.dataset.index];
-  //   console.log(currentProject);
-  const task = new TaskClass(
-    "Read Top",
-    "Dom manipulation and its application",
-    new Date(),
-    currentProject.name,
-    2
-  );
-  currentProject.projectsItem = task;
+  // const currentProject = Project.projects[ev.target.dataset.index];
+  // //   console.log(currentProject);
+  // const task = new TaskClass(
+  //   "Read Top",
+  //   "Dom manipulation and its application",
+  //   new Date(),
+  //   currentProject.name,
+  //   2
+  // );
+  // currentProject.projectsItem = task;
   // console.log(currentProject.projectsItem);
+  // alert("hy");
+  const modal = document.querySelector(".modal");
+  modal.showModal();
 }
 
 export function addItemEventLister(addItem) {
   addItem.forEach((item) => item.addEventListener("click", createTaskFunction));
 }
+
+createTaskFunction();
