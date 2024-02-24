9bc87b9dd29dc69bb050a405b54f245b438d0866
Adem Kedir <ademkedir724@gmail.com>
2024-02-24 21:12:27 +0300

    use eslint on dom directory

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import "./style/style.scss";
-import "./integrate/projectCon";
-import { displayAllProject } from "./integrate/createCardContainer";
+import './style/style.scss';
+import './integrate/projectCon';
+import { displayAllProject } from './integrate/createCardContainer';
 
-const removeLocalStorage = document.querySelector(".removeLocalStorage");
-removeLocalStorage.addEventListener("click", () => {
-  console.log("remove Local storage");
+const removeLocalStorage = document.querySelector('.removeLocalStorage');
+removeLocalStorage.addEventListener('click', () => {
+  console.log('remove Local storage');
   localStorage.clear();
   window.location.reload();
 });
 
-const allTasks = document.querySelector(".all-tasks");
-allTasks.addEventListener("click", () => {
+const allTasks = document.querySelector('.all-tasks');
+allTasks.addEventListener('click', () => {
   displayAllProject();
 });
--- src/integrate/taskCreator.js
+++ src/integrate/taskCreator.js
@@ -1,56 +1,57 @@
-import { Project } from "../app/Project";
-import { renderTasks } from "./createCardContainer.js";
+import Project from '../app/Project';
+import { renderTasks } from './createCardContainer';
+
+export function clearForm(modal) {
+  const tittle = document.querySelector('#tittle');
+  const description = document.querySelector('#description');
+  const dueDate = document.querySelector('#due-date');
+  tittle.value = '';
+  description.value = '';
+  dueDate.value = '';
+  modal.close();
+}
 
 function createTaskFunction(ev) {
-  let currentIndex = ev.target.dataset.index;
-  const modal = document.querySelector(".modal");
-  const formData = document.querySelector(".item-form");
+  const currentIndex = ev.target.dataset.index;
+  const modal = document.querySelector('.modal');
+  const formData = document.querySelector('.item-form');
 
-  const cancelTask = document.querySelector(".cancel-task");
-  const addTask = document.querySelector(".add-task");
+  const cancelTask = document.querySelector('.cancel-task');
+  const addTask = document.querySelector('.add-task');
   modal.showModal();
 
-  cancelTask.textContent = "Cancel";
-  addTask.textContent = "Add Task";
-  cancelTask.addEventListener("click", () => {
+  cancelTask.textContent = 'Cancel';
+  addTask.textContent = 'Add Task';
+
+  cancelTask.addEventListener('click', () => {
     clearForm(modal);
     modal.close();
   });
 
   addTask.addEventListener(
-    "click",
+    'click',
     () => {
-      let dataInput = Object.fromEntries(new FormData(formData));
+      const dataInput = Object.fromEntries(new FormData(formData));
       clearForm(modal);
-      if (dataInput.tittle != "") {
+      if (dataInput.tittle !== '') {
         const currentProject = Project.setProjects[currentIndex];
-        import("./../app/task.js").then(({ TaskClass }) => {
+        import('../app/task').then(({ TaskClass }) => {
           const task = new TaskClass(
             dataInput.tittle,
             dataInput.description,
-            new Date(dataInput["due-date"]),
+            new Date(dataInput['due-date']),
             currentProject.name,
-            dataInput.priority
+            dataInput.priority,
           );
           currentProject.projectsItem = task.getObject();
           renderTasks(currentIndex);
         });
       }
     },
-    { once: true }
+    { once: true },
   );
 }
 
 export function addItemEventLister(addItem) {
-  addItem.forEach((item) => item.addEventListener("click", createTaskFunction));
-}
-
-export function clearForm(modal) {
-  const tittle = document.querySelector("#tittle");
-  const description = document.querySelector("#description");
-  const dueDate = document.querySelector("#due-date");
-  tittle.value = "";
-  description.value = "";
-  dueDate.value = "";
-  modal.close();
+  addItem.forEach((item) => item.addEventListener('click', createTaskFunction));
 }
