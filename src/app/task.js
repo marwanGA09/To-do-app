6ac5cd5a02ffc89ecedc320fb2009a5557431953
Adem Kedir <ademkedir724@gmail.com>
2024-02-24 21:11:13 +0300

    use eslint on app project

--- src/app/task.js
+++ src/app/task.js
@@ -1,37 +1,49 @@
-export class TaskClass {
+/* eslint-disable grouped-accessor-pairs */
+export default class TaskClass {
   #tittle;
+
   #description;
+
   #date;
+
   #category;
+
   #priority;
+
   #isDone;
+
   constructor(
     tittle,
-    description = "",
+    description,
     date,
     category,
     priority = 3,
-    isDone = false
+    isDone = false,
   ) {
     this.#tittle = tittle;
-    this.#description = description;
+    this.#description = description === '' ? '' : description;
     this.#date = date;
     this.#category = category;
     this.#priority = priority;
     this.#isDone = isDone;
   }
+
   isDoneChanger() {
-    this.isDone = this.isDone ? false : true;
+    this.isDone = !this.isDone;
   }
+
   updateTittle(tittle) {
     this.tittle = tittle;
   }
+
   updateDescription(description) {
     this.description = description;
   }
+
   updateDate(date) {
     this.date = date;
   }
+
   updateCategory(category) {
     this.category = category;
   }
@@ -39,30 +51,39 @@ export class TaskClass {
   get isDone() {
     return this.#isDone;
   }
+
   set isDone(val) {
     this.#isDone = val;
   }
+
   set tittle(val) {
     this.#tittle = val;
   }
+
   set description(val) {
     this.#description = val;
   }
+
   set date(val) {
     this.#date = val;
   }
+
   get tittle() {
     return this.#tittle;
   }
+
   get description() {
     return this.#description;
   }
+
   get date() {
     return this.#date;
   }
+
   get category() {
     return this.#category;
   }
+
   get priority() {
     return this.#priority;
   }
